04bfdfdb741c099d2638e7fa227f0da4c8d892ac
AntonW <awiklund76@gmail.com>
2024-02-09 16:16:10 +0200

    Add OrderJWTFilter and update security configurations

--- backend/microservices/order-ms/src/main/java/com/gritlabstudent/order/ms/config/OrderSecurityConfig.java
+++ backend/microservices/order-ms/src/main/java/com/gritlabstudent/order/ms/config/OrderSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.gritlabstudent.order.ms.config;
 
+import com.gritlabstudent.order.ms.filter.OrderJWTFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,33 +23,24 @@ public class OrderSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors().and() // Enable CORS
-                .csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
-                )
-                .and()
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/orders/").permitAll() // Allow all requests to /api/orders/
-                        .requestMatchers("/api/orders/**").permitAll() // Allow all requests to /api/orders/*
-                        .requestMatchers("/ws/**").permitAll() // Allow all requests to websocket
-                        .anyRequest().permitAll() // All other requests need authentication
-                );
+        http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(
+                        exceptionHandling -> exceptionHandling
+                                .authenticationEntryPoint((request, response, authException) -> response
+                                        .sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                .authorizeHttpRequests(authorizeRequests -> {
+                    try {
+                        authorizeRequests
+                                .requestMatchers("/api/products/**").permitAll()
+                                .requestMatchers("/api/products").permitAll()
+                                .requestMatchers("/products/status/**").permitAll()
+                                .anyRequest().authenticated(); // Require authentication for all other requests
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .addFilterBefore(new OrderJWTFilter(), UsernamePasswordAuthenticationFilter.class);
+        // Build and return the configured HttpSecurity object
         return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://localhost:4200", "https://164.90.180.143:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply CORS to all paths
-        return source;
     }
 }
