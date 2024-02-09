9aec3bc64cfc9105f3aba22847a55fecb40af5f0
AntonW <awiklund76@gmail.com>
2024-02-09 16:04:19 +0200

    Add PATCH method to allowedMethods in SecurityConfig

--- backend/microservices/api-gateway/src/main/java/com/gritlabstudent/config/SecurityConfig.java
+++ backend/microservices/api-gateway/src/main/java/com/gritlabstudent/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://localhost:4200", "https://164.90.180.143:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
