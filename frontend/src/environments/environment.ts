349dc9c69aa583b66b381e2b1623b68badd81a3b
AntonW <awiklund76@gmail.com>
2024-02-09 16:53:05 +0200

    new

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   production: true,
   APIGATEWAYHOST: 'https://164.90.180.143:8443',
   baseUrl: 'https://164.90.180.143:8443',
-  WEBSOCKETHOST: 'https://164.90.180.143:8443/ws'
+  WEBSOCKETHOST: 'http://164.90.180.143:8084'
 };
