6d569d4178dca802ebff389205c2889c9cea3495
AntonW <awiklund76@gmail.com>
2024-02-09 17:06:38 +0200

    added s to websocket urlö

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   baseUrl: 'https://localhost:8443',
   APIGATEWAYHOST: 'https://localhost:8443',
-  WEBSOCKETHOST: 'http://localhost:8084'
+  WEBSOCKETHOST: 'https://localhost:8084'
 };
