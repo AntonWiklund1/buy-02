abf8b859c3f18b812dc7018c51f3facf7fe3025b
VictorRehnberg <victor.rehnberg@gmail.com>
2024-02-12 22:34:12 +0200

    added tests

---
+++ backend/microservices/order-ms/src/test/java/com/gritlab/student/order/ms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gritlab.student.order.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest()
+class OrderControllerTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
